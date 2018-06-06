a48ae48e99b33f590c4ffc0de79d8da458c96f93
f5074 <oseongryu@gmail.com>
2018-06-06 21:15:53 +0900

    Add StudyView
    Add StudyView

--- AspnetCoreStudy/Controllers/StudyController.cs
+++ AspnetCoreStudy/Controllers/StudyController.cs
@@ -12,5 +12,20 @@ namespace AspnetCoreStudy.Controllers
         {
             return View();
         }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+
+        public IActionResult CSharp()
+        {
+            return View();
+        }
+
+        public IActionResult Cpp()
+        {
+            return View();
+        }
     }
 }

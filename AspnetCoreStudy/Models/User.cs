0e0278a8f6f5fceeaee02ea377a4420d0dc58942
f5074 <oseongryu@gmail.com>
2018-06-07 00:06:37 +0900

    [Use] Migration EntityFramework2
    모델추가 못한거 commit

--- AspnetCoreStudy/Models/User.cs
+++ AspnetCoreStudy/Models/User.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AspnetCoreStudy.Models
 {
     public class User
     {
-        // 사용자 번호
+        //// 사용자 번호
+        //public int UserNo { get; set; }
+
+        //// 사용자 이름
+        //public string UserName { get; set; }
+
+
+        /// <summary>
+        /// 사용자 번호
+        /// </summary>
+        [Key]  // PK 설정
         public int UserNo { get; set; }
 
-        // 사용자 이름
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        [Required] //  Not Null 설정
         public string UserName { get; set; }
 
+        /// <summary>
+        /// 사용자 ID
+        /// </summary>
+        [Required] //  Not Null 설정
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 사용자 비밀번호
+        /// </summary>
+        [Required] //  Not Null 설정
+        public string UserPassword { get; set; }
     }
 }

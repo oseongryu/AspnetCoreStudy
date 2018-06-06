307f8f85943fc964a9c5a843d88ab48a65091688
f5074 <oseongryu@gmail.com>
2018-06-07 00:05:30 +0900

    [Use] Migration EntityFramework
    엔티티프레임워크를 사용하여 DB에 생성하는방법

---
+++ AspnetCoreStudy/DataContext/AspnetNoteDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AspnetCoreStudy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreStudy.DataContext
+{
+    public class AspnetNoteDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = myServerAddress; Database = myDataBase; User Id = myUsername;Password = myPassword;");
+        }
+    }
+}
---
+++ AspnetCoreStudy/Models/Note.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspnetCoreStudy.Models
+{
+    public class Note
+    {
+        /// <summary>
+        /// 게시물 번호
+        /// </summary>
+        [Key]  // PK 설정
+        public int NoteNo { get; set; }
+
+        /// <summary>
+        /// 게시물 제목
+        /// </summary>
+        [Required] //  Not Null 설정
+        public string NoteTitle { get; set; }
+
+        /// <summary>
+        /// 게시물 내용
+        /// </summary>
+        [Required] //  Not Null 설정
+        public string NoteContents { get; set; }
+
+        /// <summary>
+        /// 작성자 번호
+        /// </summary>
+        [Required] //  Not Null 설정
+        public int UserNo { get; set; }
+
+        /// <summary>
+        /// 조인 방법
+        /// </summary>
+        [ForeignKey("UserNo")]
+        public virtual User User { get; set; }
+
+    }
+}

a4bc0c30831a379749d80d5bb528405566835a35
f5074 <oseongryu@gmail.com>
2018-06-07 03:20:10 +0900

    Add AccountController,View
    회원가입 콘트롤러와 뷰 추가

---
+++ AspnetCoreStudy/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreStudy.DataContext;
+using AspnetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <returns></returns>
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+
+
+            // Check validation
+
+            // C#  Java try(SqlSession){} catch(){}
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    // 메모리 상에 올라감
+                    db.Users.Add(model);
+
+                    // sql로 저장
+                    db.SaveChanges();
+                }
+                // 돌아가는 위치
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+    }
+}
--- AspnetCoreStudy/Models/User.cs
+++ AspnetCoreStudy/Models/User.cs
@@ -20,19 +20,19 @@ namespace AspnetCoreStudy.Models
         /// <summary>
         /// 사용자 이름
         /// </summary>
-        [Required] //  Not Null 설정
+        [Required(ErrorMessage = "사용자 이름을 입력하세요.")] //  Not Null 설정
         public string UserName { get; set; }
 
         /// <summary>
         /// 사용자 ID
         /// </summary>
-        [Required] //  Not Null 설정
+        [Required(ErrorMessage = "사용자 ID을 입력하세요.")] //  Not Null 설정
         public string UserId { get; set; }
 
         /// <summary>
         /// 사용자 비밀번호
         /// </summary>
-        [Required] //  Not Null 설정
+        [Required(ErrorMessage = "사용자 비밀번호를 입력하세요.")] //  Not Null 설정
         public string UserPassword { get; set; }
     }
 }

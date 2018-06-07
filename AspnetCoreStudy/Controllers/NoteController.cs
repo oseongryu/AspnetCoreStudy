b70b2ecda6c39611943e68db3898232b572e7533
f5074 <oseongryu@gmail.com>
2018-06-07 20:04:41 +0900

    [Function]View Board List
    게시판 리스트 출력 기능 추가

---
+++ AspnetCoreStudy/Controllers/NoteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreStudy.DataContext;
+using AspnetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시판 리스트
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            using (var db = new AspnetNoteDbContext())
+            {
+                //ToList로 선언해서 리스트로 
+                var list = db.Notes.ToList();
+                return View(list);
+            }
+        }
+
+        /// <summary>
+        /// 게시물 추가
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}

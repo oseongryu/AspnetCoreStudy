49d2bcb269a787b69289ff805c4cc67fe61c141e
f5074 <oseongryu@gmail.com>
2018-06-07 20:44:46 +0900

    [Fuction]Add Board
    게시판 글 추가 기능 추가

--- AspnetCoreStudy/Controllers/NoteController.cs
+++ AspnetCoreStudy/Controllers/NoteController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspnetCoreStudy.DataContext;
 using AspnetCoreStudy.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -18,6 +19,11 @@ namespace AspnetCoreStudy.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
+            if(HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
 
             using (var db = new AspnetNoteDbContext())
             {
@@ -33,7 +39,43 @@ namespace AspnetCoreStudy.Controllers
         /// <returns></returns>
         public IActionResult Add()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
+            if (ModelState.IsValid)
+            {
+                using(var db = new AspnetNoteDbContext())
+                {
+                    db.Notes.Add(model);
+
+
+                    //var result =  db.SaveChanges(); // commit
+
+                    if(db.SaveChanges() > 0)
+                    {
+                        //동일한 위치의 Index로 이동
+                        return Redirect("Index");
+                        //return RedirectToAction("Index","Home");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "게시물을 저장할 수 없습니다.");
+            }
+            return View(model);
         }
 
         /// <summary>
@@ -42,6 +84,11 @@ namespace AspnetCoreStudy.Controllers
         /// <returns></returns>
         public IActionResult Edit()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
 
@@ -51,6 +98,11 @@ namespace AspnetCoreStudy.Controllers
         /// <returns></returns>
         public IActionResult Delete()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
     }
